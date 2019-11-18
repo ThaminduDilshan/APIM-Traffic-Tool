be19d21270fc2a87362a0cdd7a2fac439a4bc286
Nipun Sampath <nipunsampath@gmail.com>
2019-11-18 23:50:44 +0530

    code refactoring and comments

--- lib/attack-tool/src/python/stolen_token.py
+++ lib/attack-tool/src/python/stolen_token.py
@@ -32,15 +32,15 @@ def generate_unique_ip():
     Returns a unique ip address
     :return: an unique ip
     """
-    global fake_generator, current_ips
+    global used_ips
 
     random.seed()
     MAX_IPV4 = ipaddress.IPv4Address._ALL_ONES
     temp_ip = ipaddress.IPv4Address._string_from_ip_int(random.randint(0, MAX_IPV4))
-    while temp_ip in current_ips:
-        temp_ip = fake_generator.ipv4()
+    while temp_ip in used_ips:
+        temp_ip = ipaddress.IPv4Address._string_from_ip_int(random.randint(0, MAX_IPV4))
 
-    current_ips.append(temp_ip)
+    used_ips.append(temp_ip)
     return temp_ip
 
 
@@ -55,22 +55,23 @@ def generate_cookie():
     return cookie
 
 
-def handler_scenario(scenario):
+def execute_scenario(scenario):
     """
-    Execute scenarios from the scenario pool
+    Execute a scenario from the scenario pool to simulate the usage of a stolen token
     :param scenario: A list containing a scenario
     :return: none
     """
-    global attack_duration, protocol, host, port, payloads, user_agents, start_time,dataset_path
+    global attack_duration, protocol, host, port, payloads, user_agents, start_time, dataset_path
+
     up_time = datetime.now() - start_time
+
     if up_time.seconds < attack_duration:
+        request_target = scenario[0]
         context = scenario[1]
         version = scenario[2]
         resource_path = scenario[3]
         token = scenario[4]
         method = scenario[5]
-        request_target = scenario[0]
-        current_requests = 0
 
         request_path = "{}://{}:{}/{}/{}/{}".format(protocol, host, port, context, version, resource_path)
         random_user_agent = random.choice(user_agents)
@@ -83,57 +84,57 @@ def handler_scenario(scenario):
             if up_time.seconds >= attack_duration:
                 break
             response = util_methods.send_simple_request(request_path, method, token, random_ip, random_cookie, random_user_agent, payload=random_payload)
+
             request_string = "{},{},{},{},{},{},{}".format(datetime.now(), request_path, method, token, random_ip, random_cookie, response.status_code)
             util_methods.log(dataset_path, request_string, "a")
 
             print("Request sent with token: %s" % token, flush=True)
 
+            # sleep the process for a random period of time between 0 and 5 seconds but biased to 0
             time.sleep(generate_biased_random(0, 5, 2))
-            current_requests += 1
 
 
+# Program Execution
 if __name__ == '__main__':
     with open(os.path.abspath(os.path.join(__file__, "../../../../traffic-tool/data/runtime_data/user_scenario_pool.sav")), "rb") as scenario_file:
         scenario_pool = pickle.load(scenario_file, )
 
-    with open(os.path.abspath(os.path.join(__file__, "../../../../../config/apim.yaml")), "r") as config_file:
-        config = yaml.load(config_file, Loader=yaml.FullLoader)
-
     with open(os.path.abspath(os.path.join(__file__, "../../../../../config/attack-tool.yaml")), "r") as attack_config_file:
         attack_config = yaml.load(attack_config_file, Loader=yaml.FullLoader)
 
-    # configurations
+    # Reading configurations from attack-tool.yaml
     protocol = attack_config['general_config']['api_host']['protocol']
     host = attack_config['general_config']['api_host']['ip']
     port = attack_config['general_config']['api_host']['port']
     attack_duration = attack_config['general_config']['attack_duration']
-
     payloads = attack_config['general_config']['payloads']
     user_agents = attack_config['general_config']['user_agents']
 
-    current_ips = []
-
-    attack_tool_log_path = "../../../../../../logs/attack-tool.log"
+    # Recording column names in the dataset csv file
     dataset_path = "../../../../../../dataset/attack/stolen_token.csv"
-    start_time = datetime.now()
-
-    log_string = "[INFO] {} - Stolen token attack started ".format(start_time)
-
-    print(log_string)
-    util_methods.log(attack_tool_log_path, log_string, "a")
     util_methods.log(dataset_path, "Timestamp, Request path, Method,Access Token, IP Address, Cookie, Response Code", "w")
 
-    pool = Pool(processes=20)
+    used_ips = []
+    start_time = datetime.now()
 
+    attack_tool_log_path = "../../../../../../logs/attack-tool.log"
+    log_string = "[INFO] {} - Stolen token attack started ".format(start_time)
+    print(log_string)
+    util_methods.log(attack_tool_log_path, log_string, "a")
+
+    process_pool = Pool(processes=20)
+
+    # Executing scenarios until the attack duration elapses
     while True:
         time_elapsed = datetime.now() - start_time
         if time_elapsed.seconds >= attack_duration:
-            log_string = "[INFO] {} - Attack terminated successfully. Time elapsed: {} minutes".format(datetime.now(),time_elapsed.seconds / 60.0)
+            log_string = "[INFO] {} - Attack terminated successfully. Time elapsed: {} minutes".format(datetime.now(), time_elapsed.seconds / 60.0)
             print(log_string)
             util_methods.log(attack_tool_log_path, log_string, "a")
             break
         else:
-            pool.map(handler_scenario, scenario_pool)
+            process_pool.map(execute_scenario, scenario_pool)
 
-    pool.close()
-    pool.join()
+    # closes the process pool and wait for the processes to finish
+    process_pool.close()
+    process_pool.join()
